ee2880a1f98955becde6e5a718ddcf6248530550
s244926 <s244926@studenti.polito.it>
2020-06-08 00:17:52 +0200

    Progetto Base

---
+++ src/main/java/it/polito/tdp/seriea/FXMLController.java
@@ -0,0 +1,52 @@
+package it.polito.tdp.seriea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.seriea.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<?> boxSeason;
+
+    @FXML
+    private ChoiceBox<?> boxTeam;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void handleCarica(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDomino(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxSeason != null : "fx:id=\"boxSeason\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert boxTeam != null : "fx:id=\"boxTeam\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
